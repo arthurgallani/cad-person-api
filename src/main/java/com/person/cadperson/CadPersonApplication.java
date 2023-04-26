1e81fafb985eb6d632d860f38354633e3e57a927
arthurgallani <arthurgallanialves@gmail.com>
2023-04-26 15:35:12 -0300

    init packages structure

---
+++ src/main/java/com/person/cadperson/CadPersonApplication.java
@@ -0,0 +1,13 @@
+package com.person.cadperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadPersonApplication.class, args);
+	}
+
+}
